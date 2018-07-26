ee032fa3642962d369b6ae412741df710fd37ebe
Superman <703353105@qq.com>
2018-07-26 19:33:11 +0800

    修改代码

--- CircleAnimation/ViewController.m
+++ CircleAnimation/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "CircleAnimationView.h"
+#import "UIView+Extensions.h"
+
+#define SCREEN_WIDTH ([[UIScreen mainScreen] bounds].size.width)
+#define SCREEN_HEIGHT ([[UIScreen mainScreen] bounds].size.height)
 
 @interface ViewController ()
+@property (nonatomic, strong) CircleAnimationView *circleProgressView;
 
 @end
 
@@ -17,6 +23,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.circleProgressView = [[CircleAnimationView alloc] initWithFrame:CGRectMake(40.f, 70.f, SCREEN_WIDTH - 80.f, SCREEN_WIDTH - 80.f)];
+    self.circleProgressView.bgImage = [UIImage imageNamed:@"backgroundImage"];
+    
+    self.circleProgressView.percent = 0.f;
+    [self.view addSubview:self.circleProgressView];
+    
+    UIButton *stareButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    stareButton.frame = CGRectMake(10.f, self.circleProgressView.bottom + 50.f, SCREEN_WIDTH - 20.f, 38.f);
+    [stareButton addTarget:self action:@selector(onStareButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [stareButton setTitle:@"Stare Animation" forState:UIControlStateNormal];
+    [stareButton setBackgroundColor:[UIColor lightGrayColor]];
+    stareButton.layer.masksToBounds = YES;
+    stareButton.layer.cornerRadius = 4.f;
+    [self.view addSubview:stareButton];
+}
+- (void)onStareButtonClick {
+    
+    self.circleProgressView.percent = 90.f;
 }
 
 
